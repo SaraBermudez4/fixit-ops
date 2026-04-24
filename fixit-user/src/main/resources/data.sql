257238b5936692a99c499f493b90bdbd5c5a7a7e
FelipeUsuga1 <pipeusuga48501@gmail.com>
2026-04-23 19:01:14 -0500

    Dockerizacion completa, autenticacion y asignacion automatica funcionando

--- fixit-user/src/main/resources/data.sql
+++ fixit-user/src/main/resources/data.sql
@@ -1,7 +1,12 @@
 INSERT INTO users.users (dni, name, last_name, email, password, phone_number, role)
-SELECT '0000000000', 'Admin', 'Fixit', 'admin@fixit.com',
-       '$2a$10$3GNo4y25G55v/ggLutS2NOyRKmURS.HajmAwfmhnqDd1OpSep4/KW',
-       '+573012171281', 'ADMIN'
+SELECT
+    '0000000000',
+    'Admin',
+    'Fixit',
+    'admin@fixit.com',
+    '$2a$10$KIm8nHRrNszphpDLo80V6e7z9ufobmsreqR0aVWBtBMvvYC7e9Qza',
+    '+573001234567',
+    'ADMIN'
     WHERE NOT EXISTS (
     SELECT 1 FROM users.users WHERE email = 'admin@fixit.com'
-);
+);docker exec -it fixit-postgres psql -U fixit_user -d fixit_db
